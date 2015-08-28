ad35bd71a51bf6b446a92b5262706bd0e877cf04
Simon Gomizelj <simongmzlj@gmail.com>
2015-08-28 13:47:41 -0400

    Add missing file

---
+++ nss.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stddef.h>
+#include <nss.h>
+#include <netdb.h>
+
+enum nss_status _nss_etcd_gethostbyname2_r(const char *name,
+                                           int af,
+                                           struct hostent *result,
+                                           char *buffer,
+                                           size_t buflen,
+                                           int *errnop,
+                                           int *h_errnop);
